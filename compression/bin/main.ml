a78092046634f556ac5282c91135831822fd1c7d
Daniel Pinson <daniel.pinson19@gmail.com>
2023-03-24 17:46:15 +0100

    Setup de base pour tester la bibliothèque ocamlGsl

--- compression/bin/main.ml
+++ compression/bin/main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "Hello, World!"
+(*let () = print_endline "Hello, World!"*)
+print_string "\n";;
+
+open Gsl;;
+
+let m = Matrix.create ?init:(Some 1.) 2 2 in
+print_float(Matrix.get m 0 0) ;
+print_float(Matrix.get m 0 1) ;
+print_float(Matrix.get m 1 0) ;
+print_float(Matrix.get m 1 1) ; print_string "\n";;
+
+print_string "Hello, World\n";;
